b7a931bf9da9572cd223ad0f65efba153b9cff29
Marijo <marijo.novosel@predavaci.algebra.hr>
2018-01-30 12:15:57 +0100

    init

--- app/src/main/java/org/bubulescu/viewpager/MainActivity.java
+++ app/src/main/java/org/bubulescu/viewpager/MainActivity.java
@@ -1,13 +1,25 @@
 package org.bubulescu.viewpager;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.vpPager);
+        tabLayout = findViewById(R.id.tlTabs);
+
+        FragmentAdapter fragmentAdapter = new FragmentAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(fragmentAdapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
